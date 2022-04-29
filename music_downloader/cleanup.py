6222201001e5aa40234b49c226dc343fbf0bf5e2
aubustou <survivalfr@yahoo.fr>
2022-04-29 12:29:44 +0200

    Move everything python in a package

--- music_downloader/cleanup.py
+++ music_downloader/cleanup.py
@@ -1,13 +1,16 @@
 from pathlib import Path
 
-
 for folder in Path("K:\Mp3\Boxon").iterdir():
 
     if not folder.is_dir():
         continue
     for music_folder in folder.iterdir():
         files = list(music_folder.glob("*"))
-        if len(files) == 2 and (music_folder / "_ - thumbnail.jpg").exists() and list(music_folder.glob("*.json")):
+        if (
+            len(files) == 2
+            and (music_folder / "_ - thumbnail.jpg").exists()
+            and list(music_folder.glob("*.json"))
+        ):
             print(f"{str(music_folder)} - {len(files)}")
             try:
                 for file_ in music_folder.iterdir():
